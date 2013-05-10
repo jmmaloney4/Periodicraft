dc9b9522bbcb545203591d1272d9999b62575f06
Jack Maloney <jmmaloney4@gmail.com>
2013-05-09 20:24:06 -0500

    Testing

---
+++ structure/ComponentWoodlandHut.java
@@ -0,0 +1,17 @@
+package mods.Periodicraft.structure;
+
+import java.util.List;
+import java.util.Random;
+
+import net.minecraft.world.gen.structure.ComponentVillageStartPiece;
+import net.minecraft.world.gen.structure.StructureComponent;
+import net.minecraft.world.gen.structure.StructureVillagePieces;
+
+public abstract class ComponentWoodlandHut extends StructureComponent {
+
+	protected ComponentWoodlandHut(int par1) {
+		super(par1);
+		
+	}
+
+}
---
+++ structure/ComponentWoodlandHutHouse.java
@@ -0,0 +1,22 @@
+package mods.Periodicraft.structure;
+
+import java.util.Random;
+
+import net.minecraft.world.World;
+import net.minecraft.world.gen.structure.StructureBoundingBox;
+
+public class ComponentWoodlandHutHouse extends ComponentWoodlandHut {
+
+	protected ComponentWoodlandHutHouse(int par1) {
+		super(par1);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean addComponentParts(World world, Random random,
+			StructureBoundingBox structureboundingbox) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
