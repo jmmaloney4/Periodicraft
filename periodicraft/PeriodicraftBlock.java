abd990584d72cb3d3e1fc6f8af59ef8d706cb429
Jack Maloney <jmmaloney4@gmail.com>
2013-12-02 16:28:01 -0600

    last touches on world gen

--- periodicraft/PeriodicraftBlock.java
+++ periodicraft/PeriodicraftBlock.java
@@ -78,6 +78,16 @@ public abstract class PeriodicraftBlock extends Block {
 		return this;
 	}
 
+	public Block addToNetherGen(EnumBlockRarity rarity) {
+		Periodicraft.WGen.addToNetherGen(new WorldGenBlock(rarity, this));
+		return this;
+	}
+
+	public Block addToEndGen(EnumBlockRarity rarity) {
+		Periodicraft.WGen.addToEndGen(new WorldGenBlock(rarity, this));
+		return this;
+	}
+
 	// public Block addToSurfaceGen(int VPC, int BPV, int MH) {
 	// Periodicraft.WGen
 	// .addToSurfaceGen(new WorldGenBlock(VPC, BPV, MH, this));
