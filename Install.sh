8b609079e78dafe818cadc658a3b9981d564bca1
Jack Maloney <jmmaloney4@gmail.com>
2013-12-03 19:01:49 -0600

    fixes to install.sh

--- Install.sh
+++ Install.sh
@@ -18,8 +18,12 @@ fi
 echo $installfolder
 
 curl https://github.com/Periodicraft-Dev/Periodicraft-Minecraft-Mod/archive/master.tar.gz > ./install/repo.tar.gz
+
 curl http://files.minecraftforge.net/maven/net/minecraftforge/forge/1.6.4-9.11.1.953/forge-1.6.4-9.11.1.953-installer.jar > ./installer.jar
+
 curl http://files.minecraftforge.net/maven/net/minecraftforge/forge/1.6.4-9.11.1.953/forge-1.6.4-9.11.1.953-src.zip > ./install/src.zip
+
+curl https://s3.amazonaws.com/Minecraft.Download/launcher/Minecraft.jar > ./install/minecraft.jar
 
 echo "\n>>>> Please Choose your correct minecraft folder and install forge for client, DO NOT CANCEL, or installation will fail"
 
@@ -39,12 +43,12 @@ mv ./periodicraft/ ./forge/mcp/src/minecraft/mods
 chmod +x ./forge/reobfuscate.sh
 chmod +x ./forge/recompile.sh
 
-./reobfuscate.sh
-./recompile.sh
+./forge/mcp/reobfuscate.sh
+./forge/mcp/recompile.sh
 
-jar -x
+mkdir $installfolder/mods
 
-mv ./forge/reobf/mods $installfolder/
+mv ./forge/reobf/mods $installfolder/mods
 
 # USE TAR tar -xf ./FILE.tar.xz
 
